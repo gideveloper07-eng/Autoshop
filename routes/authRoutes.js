c7e3373034308331b89926ad6e996797019927a4
jsaurabh01997 <jsaurabh01997@gmail.com>
2026-05-23 14:59:58 +0530

    update for firebse

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,5 +10,60 @@ const verifyToken = require("../middleware/authMiddleware");
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.post("/save-fcm-token", async (req, res) => {
+  let pool;
 
+  try {
+    const decoded = decodeToken(req);
+
+    if (!decoded) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    const { database: databaseName, userId } = decoded;
+
+    const { token } = req.body;
+
+    if (!token) {
+      return res.status(400).json({
+        success: false,
+        message: "FCM token required",
+      });
+    }
+
+    pool = await openPool(databaseName);
+
+    await pool
+      .request()
+
+      .input("user_id", sql.NVarChar, userId)
+
+      .input("fcm_token", sql.NVarChar(sql.MAX), token).query(`
+          INSERT INTO app_user_devices (
+            user_id,
+            fcm_token
+          )
+          VALUES (
+            @user_id,
+            @fcm_token
+          )
+        `);
+
+    return res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.error("SAVE FCM ERROR:", err.message);
+
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  } finally {
+    if (pool) await pool.close();
+  }
+});
 module.exports = router;
